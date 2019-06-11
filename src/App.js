cbe3eda8f42f50cee4bb1a1827890f14ae52779b
kalariya-parikshith <k.parikshith93@gmail.com>
2019-06-11 13:14:30 +0530

    cloned

--- src/App.js
+++ src/App.js
@@ -13,34 +13,34 @@ import SimpleTable from './Components/SimpleTable';
 import DoughNut from './Components/DoughNut';
 
 export let data1 = [
-  { x: 'VP Approval', y: 7 },
-  { x: 'Vendor Submit ', y: 3 },
-  { x: 'Interview', y: 5 },
-  { x: 'Offer', y: 1 }
+	{ x: 'VP Approval', y: 7 },
+	{ x: 'Vendor Submit ', y: 3 },
+	{ x: 'Interview', y: 5 },
+	{ x: 'Offer', y: 1 }
 ];
 
 /**
  * Bar sample
  */
 export default class App extends SampleBase {
-  render() {
-    return (
-      <div>
-        <SideDrawer />
-        <main
-          style={{ marginLeft: '26.1%', marginTop: '10%', marginRight: '8%' }}
-        >
-          <SimpleCard>
-            <DoughNut />
-          </SimpleCard>
-          <SimpleCard>
-            <Bar />
-          </SimpleCard>
-          <SimpleTable />
-          {/* <Bar />
+	render() {
+		return (
+			<div>
+				<SideDrawer />
+				<main
+					style={{ marginLeft: '26.1%', marginTop: '10%', marginRight: '8%' }}
+				>
+					<SimpleCard>
+						<DoughNut />
+					</SimpleCard>
+					<SimpleCard>
+						<Bar />
+					</SimpleCard>
+					<SimpleTable />
+					{/* <Bar />
           <Funnel /> */}
-        </main>
-      </div>
-    );
-  }
+				</main>
+			</div>
+		);
+	}
 }
